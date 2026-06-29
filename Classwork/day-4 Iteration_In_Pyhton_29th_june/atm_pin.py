96ba53a79d6534ebceb58dc8c07ff19bfa6654d9
Aman Chauhan <amanfaceyou@gmail.com>
2026-06-29 10:10:55 +0530

    Atm Pin Verification Program

---
+++ Classwork/day-4 Iteration_In_Pyhton_29th_june/atm_pin.py
@@ -0,0 +1,10 @@
+correct_pin = 4589
+
+while True:
+    pin = int(input("Enter PIN: "))
+
+    if pin == correct_pin:
+        print("Access Granted")
+        break
+    else:
+        print("Incorrect PIN")
