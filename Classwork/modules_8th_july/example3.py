d9183334365793e4c63cf4cc1c8c7ce668da29f9
krishna <eemail.krishna@gmail.com>
2026-07-08 19:16:14 +0530

    8Th july modules tutorial

---
+++ Classwork/modules_8th_july/example3.py
@@ -0,0 +1,14 @@
+# importing Module
+from numericcalculation import calculate_sum
+
+# -------------------------
+
+num1 = 5
+num2 = 10
+
+print("sum of",num1,"and",num2,"is : ",calculate_sum(num1,num2))
+# -----------------------------------
+m = 50
+n = 40
+
+print("Difference of",m,"and",n,"is : ",calculate_difference(m,n))
---
+++ Classwork/modules_8th_july/numericcalculation.py
@@ -0,0 +1,16 @@
+# Function to calculate the sum of two numbers
+def calculate_sum(num1, num2):
+    return num1 + num2
+# Function to calculate the difference of two numbers
+def calculate_difference(num1, num2):
+    return num1 - num2
+
+# Function to calculate the product of two numbers
+def calculate_product(num1, num2):
+    return num1 * num2
+# Function to calculate the quotient of two numbers
+def calculate_division(num1, num2):
+    return num1 / num2
+# Function to calculate the remainder of a number
+def calculate_remainder(num1, num2):
+    return num1 % num2
